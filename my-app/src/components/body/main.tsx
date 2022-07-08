9e531e0950f4c84f18e8a6f7796dfc0e8b8754cf
acarneiro <acarneiro1413@gmail.com>
2022-07-07 20:53:32 -0400

    added icons to render on page, logs message on browser

--- my-app/src/components/body/main.tsx
+++ my-app/src/components/body/main.tsx
@@ -1,11 +1,27 @@
+import {useEffect, useState} from 'react'
 import { ChoicesBody } from "./choicesBody"
+import { randomizer, returnsWinLoseTie } from './utils'
 
 export const Main = () => {
+  const [userChoice, setUserChoice] = useState("")
+  const computer = randomizer() as string
+
+  useEffect(() => {
+
+    const user = userChoice
+    
+    const result = returnsWinLoseTie(user, computer)
+    console.log(result?.value);
+    console.log(result?.wlt);    
+
+  }, [userChoice])
+
   return (
     <body>
       <div>
         <p>Please select one of five options!</p>
-        <ChoicesBody />
+        <ChoicesBody setUserChoice={setUserChoice} />
+        
       </div>
     </body>
   )
